3531aede1612bc5543b98329e2eb1b7bd582578c
Jarne <descheerderjarne@gmail.com>
2025-04-15 09:49:40 +0200

    Anim2Pages
    Animation de changement de page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,5 +4,13 @@ var link = document.querySelector(".pageTransition");
 link.addEventListener("click", pageTransition);
 
 function pageTransition(e){
+
+    var linkHref = this.href;
+
+    document.body.classList.add("pageAnimation");
+    document.body.addEventListener("animationend", function(){
+        window.location = linkHref;
+        // window.location = URL de la page;
+    });
     e.preventDefault();
 }
