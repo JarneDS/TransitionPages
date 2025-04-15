4c61def20b4bf192f0b820fa54cc99ccb98b988e
Jarne <descheerderjarne@gmail.com>
2025-04-15 10:46:26 +0200

    exerciceAnimation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var link = document.querySelector(".pageTransition");
+/*var link = document.querySelector(".pageTransition");
 link.addEventListener("click", pageTransition);
 
 function pageTransition(e){
@@ -11,6 +11,18 @@ function pageTransition(e){
     document.body.addEventListener("animationend", function(){
         window.location = linkHref;
         // window.location = URL de la page;
+    });
+    e.preventDefault();
+}*/
+
+var button = document.querySelector(".cta");
+button.addEventListener("click", animation);
+
+function animation(e) {
+    var transition = document.querySelector(".transition");
+    transition.classList.add("anim-trans");
+    transition.addEventListener("animationend", function(){
+        transition.classList.remove("anim-trans");
     });
     e.preventDefault();
 }
